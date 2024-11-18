daea82ad449e0f9e0def083ddb2be742d23b6dd8
Adeday00 <118217404+Adeday00@users.noreply.github.com>
2024-11-18 10:08:32 -0500

    Updates

---
+++ precache-manifest.8681e87c26ec6f9298d3ec35240be428.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae533122c634353b3b94f12026b4067",
+    "url": "/todo-list-app/index.html"
+  },
+  {
+    "revision": "db9912d56cb7b65a0b87",
+    "url": "/todo-list-app/static/css/main.4ebd3a5a.chunk.css"
+  },
+  {
+    "revision": "119d60af52a2e66793c6",
+    "url": "/todo-list-app/static/js/2.979655ed.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/todo-list-app/static/js/2.979655ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e565ab24114c685cc83f",
+    "url": "/todo-list-app/static/js/3.a3a41f57.chunk.js"
+  },
+  {
+    "revision": "db9912d56cb7b65a0b87",
+    "url": "/todo-list-app/static/js/main.6185f29c.chunk.js"
+  },
+  {
+    "revision": "8ccce412e01977c23d53",
+    "url": "/todo-list-app/static/js/runtime-main.2fc83bf4.js"
+  }
+]);
